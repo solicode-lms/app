27db9c2e67f95807c6d2519cd33675f6f5876f68
ESSARRAJ <essarraj.fouad@gmail.com>
2024-12-21 15:27:23 +0100

    save

--- modules/PkgCompetences/resources/views/layouts/_sidebar.blade.php
+++ modules/PkgCompetences/resources/views/layouts/_sidebar.blade.php
@@ -11,37 +11,37 @@
     <ul class="nav nav-treeview">
         <li class="nav-item">
             <a href="{{ route('categorieTechnologies.index') }}" class="nav-link {{ Request::is('PkgCompetences/categorieTechnologies') ? 'active' : '' }}">
-                <i class="nav-icon fas fa-table"></i>
+                <i class="nav-icon fas fa-bolt"></i>
                 <p>CategorieTechnologies</p>
             </a>
         </li>
         <li class="nav-item">
             <a href="{{ route('competences.index') }}" class="nav-link {{ Request::is('PkgCompetences/competences') ? 'active' : '' }}">
-                <i class="nav-icon fas fa-user"></i>
+                <i class="nav-icon fas fa-brain"></i>
                 <p>Competences</p>
             </a>
         </li>
         <li class="nav-item">
             <a href="{{ route('filieres.index') }}" class="nav-link {{ Request::is('PkgCompetences/filieres') ? 'active' : '' }}">
-                <i class="nav-icon fas fa-table"></i>
+                <i class="nav-icon fas fa-book"></i>
                 <p>Filieres</p>
             </a>
         </li>
         <li class="nav-item">
             <a href="{{ route('modules.index') }}" class="nav-link {{ Request::is('PkgCompetences/modules') ? 'active' : '' }}">
-                <i class="nav-icon fas fa-table"></i>
+                <i class="nav-icon fas fa-puzzle-piece"></i>
                 <p>Modules</p>
             </a>
         </li>
         <li class="nav-item">
             <a href="{{ route('niveauCompetences.index') }}" class="nav-link {{ Request::is('PkgCompetences/niveauCompetences') ? 'active' : '' }}">
-                <i class="nav-icon fas fa-table"></i>
+                <i class="nav-icon fas fa-bars"></i>
                 <p>NiveauCompetences</p>
             </a>
         </li>
         <li class="nav-item">
             <a href="{{ route('technologies.index') }}" class="nav-link {{ Request::is('PkgCompetences/technologies') ? 'active' : '' }}">
-                <i class="nav-icon fas fa-table"></i>
+                <i class="nav-icon fas fa-bolt"></i>
                 <p>Technologies</p>
             </a>
         </li>
--- modules/PkgUtilisateurs/resources/views/layouts/_sidebar.blade.php
+++ modules/PkgUtilisateurs/resources/views/layouts/_sidebar.blade.php
@@ -11,37 +11,37 @@
     <ul class="nav nav-treeview">
         <li class="nav-item">
             <a href="{{ route('apprenants.index') }}" class="nav-link {{ Request::is('PkgUtilisateurs/apprenants') ? 'active' : '' }}">
-                <i class="nav-icon fas fa-table"></i>
+                <i class="nav-icon fas fa-user-graduate"></i>
                 <p>Apprenants</p>
             </a>
         </li>
         <li class="nav-item">
             <a href="{{ route('formateurs.index') }}" class="nav-link {{ Request::is('PkgUtilisateurs/formateurs') ? 'active' : '' }}">
-                <i class="nav-icon fas fa-table"></i>
+                <i class="nav-icon fas fa-chalkboard-teacher"></i>
                 <p>Formateurs</p>
             </a>
         </li>
         <li class="nav-item">
             <a href="{{ route('groupes.index') }}" class="nav-link {{ Request::is('PkgUtilisateurs/groupes') ? 'active' : '' }}">
-                <i class="nav-icon fas fa-table"></i>
+                <i class="nav-icon fas fa-cubes"></i>
                 <p>Groupes</p>
             </a>
         </li>
         <li class="nav-item">
             <a href="{{ route('niveauxScolaires.index') }}" class="nav-link {{ Request::is('PkgUtilisateurs/niveauxScolaires') ? 'active' : '' }}">
-                <i class="nav-icon fas fa-table"></i>
+                <i class="nav-icon fas fa-graduation-cap"></i>
                 <p>NiveauxScolaires</p>
             </a>
         </li>
         <li class="nav-item">
             <a href="{{ route('specialites.index') }}" class="nav-link {{ Request::is('PkgUtilisateurs/specialites') ? 'active' : '' }}">
-                <i class="nav-icon fas fa-table"></i>
+                <i class="nav-icon fas fa-award"></i>
                 <p>Specialites</p>
             </a>
         </li>
         <li class="nav-item">
             <a href="{{ route('villes.index') }}" class="nav-link {{ Request::is('PkgUtilisateurs/villes') ? 'active' : '' }}">
-                <i class="nav-icon fas fa-table"></i>
+                <i class="nav-icon fas fa-city"></i>
                 <p>Villes</p>
             </a>
         </li>
