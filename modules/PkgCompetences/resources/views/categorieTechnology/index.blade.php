4d2e498091521452fbae1a0f839f73e51e7a6d41
ESSARRAJ <essarraj.fouad@gmail.com>
2024-12-29 14:08:37 +0100

    delete categoriesTechnology

--- modules/PkgCompetences/resources/views/categorieTechnology/index.blade.php
+++
@@ -1,7 +0,0 @@
-{{-- Ce fichier est maintenu par ESSARRAJ Fouad --}}
-
-@extends('layouts.admin')
-@section('title', curd_index_title('PkgCompetences::categorieTechnology'))
-@section('content')
-    @include('PkgCompetences::categorieTechnology._index')
-@endsection
