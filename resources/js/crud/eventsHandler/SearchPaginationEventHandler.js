fed1031d124adecad79e608fb7501bdd2d364311
ESSARRAJ <essarraj.fouad@gmail.com>
2025-01-05 13:08:42 +0100

    save

--- resources/js/crud/eventsHandler/SearchPaginationEventHandler.js
+++ resources/js/crud/eventsHandler/SearchPaginationEventHandler.js
@@ -20,6 +20,7 @@ export class SearchPaginationEventHandler {
      */
     init() {
         this.handleSearchInput();
+        this.handleFilterSubmit();
         this.handlePaginationClick();
     }
 
