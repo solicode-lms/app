3bf9b698d7b2460c9db5287b4a20895b70a3b433
ESSARRAJ <essarraj.fouad@gmail.com>
2025-01-07 14:19:23 +0100

    save

--- modules/PkgCompetences/Models/Base/BaseAppreciation.php
+++ modules/PkgCompetences/Models/Base/BaseAppreciation.php
@@ -1,35 +1,62 @@
 <?php
+// Ce fichier est maintenu par ESSARRAJ Fouad
 
 
 namespace Modules\PkgCompetences\Models\Base;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use App\Traits\HasDynamicContext;
-use App\Traits\OwnedByUser;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use App\Traits\OwnedByUser;
+use App\Traits\HasDynamicContext;
+
 use Modules\PkgCreationProjet\Models\TransfertCompetence;
 use Modules\PkgUtilisateurs\Models\Formateur;
 
+/**
+ * Classe BaseAppreciation
+ * Cette classe sert de base pour le modèle Appreciation.
+ */
 class BaseAppreciation extends Model
 {
-    use HasFactory, HasDynamicContext,OwnedByUser;
+    use HasFactory, HasDynamicContext, OwnedByUser;
 
-    protected $fillable = ['nom', 'description', 'noteMin', 'noteMax', 'formateur_id'];
+    /**
+     * Les attributs remplissables pour le modèle.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'nom', 'description', 'noteMin', 'noteMax', 'formateur_id'
+    ];
 
+    /**
+     * Relation BelongsTo pour Formateur.
+     *
+     * @return BelongsTo
+     */
     public function formateur(): BelongsTo
     {
         return $this->belongsTo(Formateur::class, 'formateur_id', 'id');
     }
 
 
-
-    public function transfertCompetences():HasMany
+    /**
+     * Relation HasMany pour TransfertCompetences.
+     *
+     * @return HasMany
+     */
+    public function transfertCompetences(): HasMany
     {
-        return $this->hasMany(TransfertCompetence::class, 'appreciation_id', 'id');
+        return $this->hasMany(TransfertCompetence::class, '_id', 'id');
     }
 
+    /**
+     * Méthode __toString pour représenter le modèle sous forme de chaîne.
+     *
+     * @return string
+     */
     public function __toString()
     {
         return $this->nom;
--- modules/PkgUtilisateurs/Models/Base/BaseApprenant.php
+++ modules/PkgUtilisateurs/Models/Base/BaseApprenant.php
@@ -6,33 +6,67 @@ namespace Modules\PkgUtilisateurs\Models\Base;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use App\Traits\OwnedByUser;
 use App\Traits\HasDynamicContext;
+
 use Modules\PkgUtilisateurs\Models\Groupe;
 use Modules\PkgUtilisateurs\Models\Nationalite;
 use Modules\PkgUtilisateurs\Models\NiveauxScolaire;
 
+/**
+ * Classe BaseApprenant
+ * Cette classe sert de base pour le modèle Apprenant.
+ */
 class BaseApprenant extends Model
 {
     use HasFactory, HasDynamicContext;
 
-    protected $fillable = ['nom', 'prenom', 'prenom_arab', 'nom_arab', 'tele_num', 'profile_image', 'matricule', 'sexe', 'actif', 'diplome', 'date_naissance', 'date_inscription', 'lieu_naissance', 'cin', 'adresse', 'groupe_id', 'niveaux_scolaire_id', 'nationalite_id'];
+    /**
+     * Les attributs remplissables pour le modèle.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'nom', 'prenom', 'prenom_arab', 'nom_arab', 'tele_num', 'profile_image', 'matricule', 'sexe', 'actif', 'diplome', 'date_naissance', 'date_inscription', 'lieu_naissance', 'cin', 'adresse', 'groupe_id', 'niveaux_scolaire_id', 'nationalite_id'
+    ];
 
-    public function groupe()
+    /**
+     * Relation BelongsTo pour Groupe.
+     *
+     * @return BelongsTo
+     */
+    public function groupe(): BelongsTo
     {
         return $this->belongsTo(Groupe::class, 'groupe_id', 'id');
     }
-    public function nationalite()
+    /**
+     * Relation BelongsTo pour Nationalite.
+     *
+     * @return BelongsTo
+     */
+    public function nationalite(): BelongsTo
     {
         return $this->belongsTo(Nationalite::class, 'nationalite_id', 'id');
     }
-    public function niveauxScolaire()
+    /**
+     * Relation BelongsTo pour NiveauxScolaire.
+     *
+     * @return BelongsTo
+     */
+    public function niveauxScolaire(): BelongsTo
     {
         return $this->belongsTo(NiveauxScolaire::class, 'niveaux_scolaire_id', 'id');
     }
 
 
 
-
+    /**
+     * Méthode __toString pour représenter le modèle sous forme de chaîne.
+     *
+     * @return string
+     */
     public function __toString()
     {
         return $this->nom;
