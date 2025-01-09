3fc91a6ed752562836e2f6768cb059c8f5b93cdd
ESSARRAJ <essarraj.fouad@gmail.com>
2025-01-09 18:52:15 +0100

    save

---
+++ modules/PkgGapp/Database/Migrations/2025_01_09_174213_create_metadata_types_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('metadata_types', function (Blueprint $table) {
+            $table->id(); // Clé primaire
+            $table->string('name'); // Nom lisible
+            $table->string('code')->unique(); // Code unique pour identification technique
+            $table->string('type'); // Stocke le type comme une chaîne
+            $table->string('scope'); // Stocke la portée comme une chaîne
+            $table->string('description')->nullable(); // Description facultative
+            $table->boolean('is_required')->default(false); // Indique si la métadonnée est obligatoire
+            $table->string('default_value')->nullable(); // Valeur par défaut
+            $table->text('validation_rules')->nullable(); // Règles de validation
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('metadata_types');
+    }
+};
