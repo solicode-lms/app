b274ee5ebaacd3210d648ee10252f4c28a754d02
ESSARRAJ <essarraj.fouad@gmail.com>
2025-01-09 15:00:21 +0100

    add package Gapp

---
+++ modules/PkgGapp/App/Providers/PkgGappServiceProvider.php
@@ -0,0 +1,12 @@
+<?php
+// Ce fichier est maintenu par ESSARRAJ Fouad
+
+
+
+namespace Modules\PkgGapp\App\Providers;
+use Modules\PkgGapp\App\Providers\Base\BasePkgGappServiceProvider;
+
+class PkgGappServiceProvider extends BasePkgGappServiceProvider
+{
+    
+}
---
+++ modules/PkgGapp/resources/views/layouts/_sidebar.blade.php
@@ -0,0 +1,18 @@
+{{-- Ce fichier est maintenu par ESSARRAJ Fouad --}}
+
+
+@accessiblePermissions([])
+@if($accessiblePermissions->isNotEmpty())
+<li class="nav-item has-treeview {{ Request::is('admin/PkgGapp*') ? 'menu-open' : '' }}">
+    <a href="#" class="nav-link nav-link {{ Request::is('admin/PkgGapp*') ? 'active' : '' }}">
+        <i class="nav-icon fas  {{__('PkgGapp::PkgGapp.icon')}}"></i>
+        <p>
+            {{__('PkgGapp::PkgGapp.name')}}
+            <i class="fas fa-angle-left right"></i>
+        </p>
+    </a>
+    <ul class="nav nav-treeview">
+    </ul>
+</li>
+@endif
+
