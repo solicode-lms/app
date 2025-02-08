eac5ff7660ab841942b48e31b485bd345283e05d
ESSARRAJ <essarraj.fouad@gmail.com>
2025-02-08 09:52:07 +0100

    save

--- modules/PkgGapp/Services/EMetadatumService.php
+++ modules/PkgGapp/Services/EMetadatumService.php
@@ -3,6 +3,8 @@
 
 namespace Modules\PkgGapp\Services;
 
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Log;
 use Modules\PkgGapp\App\Enums\FieldTypeEnum;
 use Modules\PkgGapp\Services\Base\BaseEMetadatumService;
 
@@ -32,5 +34,53 @@ class EMetadatumService extends BaseEMetadatumService
         }
       
         return $eMetadatum;
+    }
+
+      /**
+     * Override de la méthode create
+     */
+    public function create($data)
+    {
+        // Appeler la méthode parente pour exécuter l'opération de création
+        $result = parent::create($data);
+
+        // Afficher la version de Node.js dans la console
+        $this->logNodeVersion("Création d'une nouvelle entité avec ID: " . $result->id);
+
+        return $result;
+    }
+
+    /**
+     * Override de la méthode update
+     */
+    public function update($id, array $data): ?Model 
+    {
+        // Appeler la méthode parente pour exécuter l'opération de mise à jour
+        $result = parent::update($id, $data);
+
+        // Afficher la version de Node.js dans la console
+        $this->logNodeVersion("Mise à jour de l'entité avec ID: " . $id);
+
+        return $result;
+    }
+
+   
+
+    /**
+     * Exécute une commande Node.js pour afficher la version dans la console.
+     */
+    private function logNodeVersion($message)
+    {
+
+        $model_name = "Competence";
+        Log::info("Génération de CRUD pour la model : {$model_name}");
+        $nodeCommand = "gapp make:crud Competence ../";
+
+        // Exécuter la commande (compatible Windows et Linux)
+        if (strtoupper(substr(PHP_OS, 0, 3)) === 'WIN') {
+            pclose(popen('start /B ' . $nodeCommand, 'r'));
+        } else {
+            shell_exec($nodeCommand . ' > /dev/null 2>&1 &');
+        }
     }
 }
