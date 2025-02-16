60d004057eec9fe01ffcf9dd692cde9d305e04c0
ESSARRAJ <essarraj.fouad@gmail.com>
2025-02-16 12:02:58 +0100

    save

--- modules/PkgCompetences/Controllers/Base/BaseNiveauDifficulteController.php
+++ modules/PkgCompetences/Controllers/Base/BaseNiveauDifficulteController.php
@@ -29,7 +29,7 @@ class BaseNiveauDifficulteController extends AdminController
     public function index(Request $request) {
         
         $this->viewState->setContextKeyIfEmpty('niveauDifficulte.index');
-        $this->viewState->init('filter.niveauDifficulte.formateur_id'  , $this->sessionState->get('formateur_id'));
+        $this->viewState->init('scope.niveauDifficulte.formateur_id'  , $this->sessionState->get('formateur_id'));
 
         // Extraire les paramètres de recherche, page, et filtres
         $niveauDifficultes_params = array_merge(
