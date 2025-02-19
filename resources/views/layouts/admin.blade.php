2bd1ccbd7594bcd40d6f33567c5b5e329e3bc12d
= <=>
2025-02-19 09:01:16 +0100

    save

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -30,7 +30,7 @@
 
         <nav class="main-header navbar navbar-expand navbar-white navbar-light">
             <!-- Left navbar links -->
-            <ul class="navbar-nav">
+            <ul class="navbar-nav" style="display: none">
               <li class="nav-item">
                 <a class="nav-link" data-widget="pushmenu" href="#" role="button"><i class="fas fa-bars"></i></a>
               </li>
@@ -44,7 +44,7 @@
     
 
               <!-- Notifications Dropdown Menu -->
-              <li class="nav-item dropdown">
+              <li class="nav-item dropdown" style="display: none">
                 <a class="nav-link" data-toggle="dropdown" href="#">
                   <i class="far fa-bell"></i>
                   <span class="badge badge-warning navbar-badge">15</span>
