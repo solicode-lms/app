090d4781418b3331c20ba761d64833e17db2432c
= <=>
2025-02-20 15:19:57 +0100

    save

--- modules/PkgRealisationProjets/Controllers/Base/BaseRealisationProjetController.php
+++ modules/PkgRealisationProjets/Controllers/Base/BaseRealisationProjetController.php
@@ -39,6 +39,8 @@ class BaseRealisationProjetController extends AdminController
         $this->viewState->setContextKeyIfEmpty('realisationProjet.index');
         if($this->sessionState->get('formateur_id')) $this->viewState->init('filter.realisationProjet.formateur_id'  , $this->sessionState->get('formateur_id'));
 
+ 
+
         // Extraire les paramètres de recherche, page, et filtres
         $realisationProjets_params = array_merge(
             $request->only(['page','sort']),
--- modules/PkgRealisationProjets/Controllers/RealisationProjetController.php
+++ modules/PkgRealisationProjets/Controllers/RealisationProjetController.php
@@ -3,14 +3,31 @@
 namespace Modules\PkgRealisationProjets\Controllers;
 
 use Illuminate\Http\Request;
-
+use Illuminate\Support\Facades\Auth;
 use Modules\PkgRealisationProjets\Controllers\Base\BaseRealisationProjetController;
 
 class RealisationProjetController extends BaseRealisationProjetController
 {
     public function index(Request $request) {
+
+        // 
         $this->viewState->setContextKeyIfEmpty('realisationProjet.index');
         if($this->sessionState->get('apprenant_id')) $this->viewState->init('filter.realisationProjet.apprenant_id'  , $this->sessionState->get('apprenant_id'));
+        
+
+        // Scope etatRéalistion de formateur 
+        if(Auth::user()->hasRole("formateur")){
+
+            $value = $this->sessionState->get('formateur_id');
+            $key = 'scope.etatsRealisationProjet.formateur_id';
+            $this->viewState->set($key, $value);
+
+            // $value = $this->sessionState->get('formateur_id');
+            // $key = 'scope.apprenant.groupes.formateurs.id';
+            // $this->viewState->set($key, $value);
+        }
+       
+
         return parent::index($request);
     }
 
