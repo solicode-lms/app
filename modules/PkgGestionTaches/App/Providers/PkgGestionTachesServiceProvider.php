232e1e31d94f2278f22cf35722dd04759e6299ce
ESSARRAJ <essarraj.fouad@gmail.com>
2025-03-01 11:12:04 +0000

    Création de module : PkgGestionTaches

---
+++ modules/PkgGestionTaches/App/Providers/PkgGestionTachesServiceProvider.php
@@ -0,0 +1,12 @@
+<?php
+// Ce fichier est maintenu par ESSARRAJ Fouad
+
+
+
+namespace Modules\PkgGestionTaches\App\Providers;
+use Modules\PkgGestionTaches\App\Providers\Base\BasePkgGestionTachesServiceProvider;
+
+class PkgGestionTachesServiceProvider extends BasePkgGestionTachesServiceProvider
+{
+    
+}
---
+++ modules/PkgGestionTaches/resources/lang/fr/PkgGestionTaches.php
@@ -0,0 +1,8 @@
+<?php
+// Ce fichier est maintenu par ESSARRAJ Fouad
+
+
+return [
+    'name' => 'PkgGestionTaches',
+    'icon' => 'fa-table'
+];
