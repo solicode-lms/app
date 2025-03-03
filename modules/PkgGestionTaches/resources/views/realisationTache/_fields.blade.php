7ecdf4c77b587d769d5fd16166bc8783de3d65da
= <=>
2025-03-03 12:46:44 +0000

    save

--- modules/PkgGestionTaches/resources/views/realisationTache/_fields.blade.php
+++ modules/PkgGestionTaches/resources/views/realisationTache/_fields.blade.php
@@ -75,12 +75,12 @@
                 {{ ucfirst(__('PkgGestionTaches::realisationTache.dateDebut')) }}
                 
                     <span class="text-danger">*</span>
-               {{$itemRealisationTache->dateDebut}}
+               
             </label>
             <input
                 name="dateDebut"
                 type="date"
-                class="form-control"
+                class="form-control datetimepicker"
                 required
                 
                 id="dateDebut"
