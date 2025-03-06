c45284d99ceba5b476edb5a28828340a4332daf4
ESSARRAJ <essarraj.fouad@gmail.com>
2025-03-06 17:15:42 +0000

    save

--- modules/PkgApprenants/Controllers/Base/BaseApprenantController.php
+++ modules/PkgApprenants/Controllers/Base/BaseApprenantController.php
@@ -41,6 +41,7 @@ class BaseApprenantController extends AdminController
         $this->viewState->setContextKeyIfEmpty('apprenant.index');
 
 
+
         // Extraire les paramètres de recherche, page, et filtres
         $apprenants_params = array_merge(
             $request->only(['page','sort']),
