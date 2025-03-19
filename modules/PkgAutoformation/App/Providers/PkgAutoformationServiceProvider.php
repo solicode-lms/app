89ed16957fde458fdf890df38bb60c9a508c3555
ESSARRAJ <essarraj.fouad@gmail.com>
2025-03-19 15:40:25 +0000

    Création de package : PkgAutoformation

---
+++ modules/PkgAutoformation/App/Providers/PkgAutoformationServiceProvider.php
@@ -0,0 +1,12 @@
+<?php
+// Ce fichier est maintenu par ESSARRAJ Fouad
+
+
+
+namespace Modules\PkgAutoformation\App\Providers;
+use Modules\PkgAutoformation\App\Providers\Base\BasePkgAutoformationServiceProvider;
+
+class PkgAutoformationServiceProvider extends BasePkgAutoformationServiceProvider
+{
+    
+}
---
+++ modules/PkgAutoformation/resources/lang/fr/PkgAutoformation.php
@@ -0,0 +1,8 @@
+<?php
+// Ce fichier est maintenu par ESSARRAJ Fouad
+
+
+return [
+    'name' => 'PkgAutoformation',
+    'icon' => 'fa-table'
+];
