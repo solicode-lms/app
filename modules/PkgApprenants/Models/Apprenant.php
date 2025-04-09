25b6e7c69065e4920663d41c31f21ff72d39128a
ESSARRAJ <essarraj.fouad@gmail.com>
2025-04-09 21:38:42 +0100

    scope apprenant inactive

--- modules/PkgApprenants/Models/Apprenant.php
+++ modules/PkgApprenants/Models/Apprenant.php
@@ -8,10 +8,37 @@ use Modules\Core\App\Traits\HasDynamicAttributes;
 use Modules\PkgApprenants\Models\Base\BaseApprenant;
 use Modules\PkgFormation\Models\AnneeFormation;
 use Modules\PkgGestionTaches\Models\RealisationTache;
+use Illuminate\Database\Eloquent\Builder;
+use Modules\Core\Services\ViewStateService;
 
 class Apprenant extends BaseApprenant
 {
     protected $appends = ['derniere_tache_terminee_ou_validation'];
+
+
+    protected static function booted()
+    {
+        // Afficher et utiliser seulement les apprenants active, else dans le cas d'un filtre 
+        static::addGlobalScope('inactif', function (Builder $builder) {
+            // Accès au ViewStateService
+            $viewState = app(ViewStateService::class);
+    
+            // Nom du modèle utilisé pour les filtres (adapter si besoin)
+            $modelName = 'apprenant';
+    
+            // Récupérer les filtres appliqués via le ViewState
+            $filters = $viewState->getFilterVariables($modelName);
+    
+            // Si le filtre 'actif' est défini, ne pas appliquer le scope
+            if (array_key_exists('actif', $filters)) {
+                return;
+            }
+    
+            // Sinon, appliquer le scope global actif = true
+            $builder->where('actif', true);
+        });
+    }
+    
 
     public function getFormateurId()
     {
