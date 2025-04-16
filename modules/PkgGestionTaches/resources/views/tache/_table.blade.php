126f252f877382c187b21c06fcb25a87128428b6
= <=>
2025-04-16 12:02:36 +0100

    save

--- modules/PkgGestionTaches/resources/views/tache/_table.blade.php
+++ modules/PkgGestionTaches/resources/views/tache/_table.blade.php
@@ -10,9 +10,10 @@
                 @endphp
                 <x-checkbox-header :bulkEdit="$bulkEdit" />
                
-                <x-sortable-column width="27.333333333333332"  field="titre" modelname="tache" label="{{ ucfirst(__('PkgGestionTaches::tache.titre')) }}" />
-                <x-sortable-column width="27.333333333333332" field="projet_id" modelname="tache" label="{{ ucfirst(__('PkgCreationProjet::projet.singular')) }}" />
-                <x-sortable-column width="27.333333333333332" field="priorite_tache_id" modelname="tache" label="{{ ucfirst(__('PkgGestionTaches::prioriteTache.singular')) }}" />
+                <x-sortable-column width="20.5"  field="titre" modelname="tache" label="{{ ucfirst(__('PkgGestionTaches::tache.titre')) }}" />
+                <x-sortable-column width="20.5" field="projet_id" modelname="tache" label="{{ ucfirst(__('PkgCreationProjet::projet.singular')) }}" />
+                <x-sortable-column width="20.5" field="priorite_tache_id" modelname="tache" label="{{ ucfirst(__('PkgGestionTaches::prioriteTache.singular')) }}" />
+                <x-sortable-column width="20.5"  field="livrables" modelname="tache" label="{{ ucfirst(__('PkgCreationProjet::livrable.plural')) }}" />
                 <th class="text-center">{{ __('Core::msg.action') }}</th>
             </tr>
         </thead>
@@ -21,21 +22,30 @@
             @foreach ($taches_data as $tache)
                 <tr id="tache-row-{{$tache->id}}">
                     <x-checkbox-row :item="$tache" :bulkEdit="$bulkEdit" />
-                    <td style="max-width: 27.333333333333332%;" class="text-truncate" data-toggle="tooltip" title="{{ $tache->titre }}" >
+                    <td style="max-width: 20.5%;" class="text-truncate" data-toggle="tooltip" title="{{ $tache->titre }}" >
                     <x-field :entity="$tache" field="titre">
                         {{ $tache->titre }}
                     </x-field>
                     </td>
-                    <td style="max-width: 27.333333333333332%;" class="text-truncate" data-toggle="tooltip" title="{{ $tache->projet }}" >
+                    <td style="max-width: 20.5%;" class="text-truncate" data-toggle="tooltip" title="{{ $tache->projet }}" >
                     <x-field :entity="$tache" field="projet">
                        
                          {{  $tache->projet }}
                     </x-field>
                     </td>
-                    <td style="max-width: 27.333333333333332%;" class="text-truncate" data-toggle="tooltip" title="{{ $tache->prioriteTache }}" >
+                    <td style="max-width: 20.5%;" class="text-truncate" data-toggle="tooltip" title="{{ $tache->prioriteTache }}" >
                     <x-field :entity="$tache" field="prioriteTache">
                        
                          {{  $tache->prioriteTache }}
+                    </x-field>
+                    </td>
+                    <td style="max-width: 20.5%;" class="text-truncate" data-toggle="tooltip" title="{{ $tache->livrables }}" >
+                    <x-field :entity="$tache" field="livrables">
+                        <ul>
+                            @foreach ($tache->livrables as $livrable)
+                                <li @if(strlen($livrable) > 30) data-toggle="tooltip" title="{{$livrable}}"  @endif>@limit($livrable, 30)</li>
+                            @endforeach
+                        </ul>
                     </x-field>
                     </td>
                     <td class="text-right text-truncate" style="max-width: 15%;">
