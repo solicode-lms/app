38ea6a8915ab0d6ac87c4bd2df2346fbd882ba2f
ESSARRAJ <essarraj.fouad@gmail.com>
2025-05-02 20:35:10 +0100

    save

--- modules/PkgGestionTaches/resources/views/realisationTache/_show.blade.php
+++ modules/PkgGestionTaches/resources/views/realisationTache/_show.blade.php
@@ -1,66 +1,73 @@
-
+{{-- Ce fichier est maintenu par ESSARRAJ Fouad --}}
 
 @section('realisationTache-show')
-<div class="card container">
-    <div class="card-header">
-        {{ __('Détails de la Réalisation de Tâche') }}
+<div class="card container shadow-sm">
+    <div class="card-header bg-primary text-white">
+        <h5 class="mb-0">{{ __('Détails de la Réalisation de Tâche') }}</h5>
     </div>
 
-    <div class="card-body row">
+    <div class="card-body">
 
-        <div class="form-group col-12 col-md-6">
-            <label>{{ ucfirst(__('PkgGestionTaches::tache.singular')) }}</label>
-            <div class="form-control-plaintext">
-                {{ $itemRealisationTache->tache ?? '-' }}
+        <h6 class="text-muted mb-3">📋 Informations Générales</h6>
+        <div class="row">
+            <div class="col-md-6 mb-3">
+                <strong>{{ ucfirst(__('PkgGestionTaches::tache.singular')) }} :</strong>
+                <div class="text-primary">{{ $itemRealisationTache->tache ?? '-' }}</div>
+            </div>
+
+            <div class="col-md-6 mb-3">
+                <strong>{{ ucfirst(__('PkgRealisationProjets::realisationProjet.singular')) }} :</strong>
+                <div class="text-primary">{{ $itemRealisationTache->realisationProjet ?? '-' }}</div>
             </div>
         </div>
 
-        <div class="form-group col-12 col-md-6">
-            <label>{{ ucfirst(__('PkgRealisationProjets::realisationProjet.singular')) }}</label>
-            <div class="form-control-plaintext">
-                {{ $itemRealisationTache->realisationProjet ?? '-' }}
+        <hr>
+
+        <h6 class="text-muted mb-3">📅 Dates</h6>
+        <div class="row">
+            <div class="col-md-3 mb-3">
+                <strong>🕑 {{ ucfirst(__('PkgGestionTaches::realisationTache.dateDebut')) }} :</strong>
+                <div>{{ $itemRealisationTache->dateDebut ?? '-' }}</div>
+            </div>
+
+            <div class="col-md-3 mb-3">
+                <strong>⏰ {{ ucfirst(__('PkgGestionTaches::realisationTache.dateFin')) }} :</strong>
+                <div>{{ $itemRealisationTache->dateFin ?? '-' }}</div>
+            </div>
+
+            <div class="col-md-6 mb-3">
+                <strong>{{ ucfirst(__('PkgGestionTaches::etatRealisationTache.singular')) }} :</strong>
+                @if (!empty($itemRealisationTache->etatRealisationTache))
+                    <span class="badge badge-info p-2">{{ $itemRealisationTache->etatRealisationTache }}</span>
+                @else
+                    <span class="text-muted">-</span>
+                @endif
             </div>
         </div>
 
-        <div class="form-group col-12 col-md-3">
-            <label>{{ ucfirst(__('PkgGestionTaches::realisationTache.dateDebut')) }}</label>
-            <div class="form-control-plaintext">
-                {{ $itemRealisationTache->dateDebut ? $itemRealisationTache->dateDebut : '-' }}
+        <hr>
+
+        <h6 class="text-muted mb-3">📝 Remarques</h6>
+        <div class="row">
+            <div class="col-md-6 mb-3">
+                <div class="card bg-light p-3">
+                    <strong>{{ ucfirst(__('PkgGestionTaches::realisationTache.remarques_formateur')) }} :</strong>
+                    <div class="mt-2">{!! nl2br(e($itemRealisationTache->remarques_formateur)) ?? '-' !!}</div>
+                </div>
+            </div>
+
+            <div class="col-md-6 mb-3">
+                <div class="card bg-light p-3">
+                    <strong>{{ ucfirst(__('PkgGestionTaches::realisationTache.remarques_apprenant')) }} :</strong>
+                    <div class="mt-2">{!! nl2br(e($itemRealisationTache->remarques_apprenant)) ?? '-' !!}</div>
+                </div>
             </div>
         </div>
 
-        <div class="form-group col-12 col-md-3">
-            <label>{{ ucfirst(__('PkgGestionTaches::realisationTache.dateFin')) }}</label>
-            <div class="form-control-plaintext">
-                {{ $itemRealisationTache->dateFin ? $itemRealisationTache->dateFin : '-' }}
-            </div>
-        </div>
+        <hr>
 
-        <div class="form-group col-12 col-md-6">
-            <label>{{ ucfirst(__('PkgGestionTaches::etatRealisationTache.singular')) }}</label>
-            <div class="form-control-plaintext">
-                {{ $itemRealisationTache->etatRealisationTache ?? '-' }}
-            </div>
-        </div>
-
-        <div class="form-group col-12 col-md-6">
-            <label>{{ ucfirst(__('PkgGestionTaches::realisationTache.remarques_formateur')) }}</label>
-            <div class="form-control-plaintext">
-                {!! nl2br(e($itemRealisationTache->remarques_formateur)) ?? '-' !!}
-            </div>
-        </div>
-
-        <div class="form-group col-12 col-md-6">
-            <label>{{ ucfirst(__('PkgGestionTaches::realisationTache.remarques_apprenant')) }}</label>
-            <div class="form-control-plaintext">
-                {!! nl2br(e($itemRealisationTache->remarques_apprenant)) ?? '-' !!}
-            </div>
-        </div>
-
-        {{-- Historique si disponible --}}
+        <h6 class="text-muted mb-3">🕘 Historique</h6>
         <div class="col-12">
-            <label>{{ ucfirst(__('PkgGestionTaches::historiqueRealisationTache.plural')) }}</label>
-
             @include('PkgGestionTaches::historiqueRealisationTache._index', [
                 'isMany' => true,
                 'edit_has_many' => false,
@@ -70,9 +77,13 @@
 
     </div>
 
-    <div class="card-footer">
-        <a href="{{ route('realisationTaches.index') }}" class="btn btn-default">{{ __('Core::msg.cancel') }}</a>
-        <a href="{{ route('realisationTaches.edit', $itemRealisationTache->id) }}" class="btn btn-info ml-2">{{ __('Core::msg.edit') }}</a>
+    <div class="card-footer text-right">
+        <a href="{{ route('realisationTaches.index') }}" class="btn btn-secondary">
+            <i class="fas fa-arrow-left"></i> {{ __('Core::msg.cancel') }}
+        </a>
+        <a href="{{ route('realisationTaches.edit', $itemRealisationTache->id) }}" class="btn btn-primary ml-2">
+            <i class="fas fa-edit"></i> {{ __('Core::msg.edit') }}
+        </a>
     </div>
 </div>
 
