b08c4a17e07481bea52931a33d47e4e68184e1a5
ESSARRAJ <essarraj.fouad@gmail.com>
2025-06-10 12:41:35 +0100

    save

--- modules/PkgValidationProjets/Models/EvaluationRealisationTache.php
+++ modules/PkgValidationProjets/Models/EvaluationRealisationTache.php
@@ -1,11 +1,19 @@
 <?php
-// Ce fichier est maintenu par ESSARRAJ Fouad
-
 
 namespace Modules\PkgValidationProjets\Models;
 use Modules\PkgValidationProjets\Models\Base\BaseEvaluationRealisationTache;
 
 class EvaluationRealisationTache extends BaseEvaluationRealisationTache
 {
+
+        /**
+     * Barème max pour la note = note de la tâche liée.
+     */
+    public function getMaxNote(): float|null
+    {
+        return $this->realisationTache?->tache?->note !== null
+            ? round($this->realisationTache->tache->note, 2)
+            : null;
+    }
 
 }
