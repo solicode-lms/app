867c160d0e8af31436f506ef19ef67c411442fb7
ESSARRAJ <essarraj.fouad@gmail.com>
2025-07-11 20:12:32 +0100

    save

---
+++ modules/PkgCreationTache/App/Providers/PkgCreationTacheServiceProvider.php
@@ -0,0 +1,12 @@
+<?php
+// Ce fichier est maintenu par ESSARRAJ Fouad
+
+
+
+namespace Modules\PkgCreationTache\App\Providers;
+use Modules\PkgCreationTache\App\Providers\Base\BasePkgCreationTacheServiceProvider;
+
+class PkgCreationTacheServiceProvider extends BasePkgCreationTacheServiceProvider
+{
+    
+}
---
+++ modules/PkgCreationTache/resources/views/layouts/_sidebar.blade.php
@@ -0,0 +1,18 @@
+{{-- Ce fichier est maintenu par ESSARRAJ Fouad --}}
+
+
+@accessiblePermissions([])
+@if($accessiblePermissions->isNotEmpty())
+<li id="menu-PkgCreationTache" class="nav-item has-treeview  {{ Request::is('admin/PkgCreationTache*') ? 'menu-open' : '' }}">
+    <a href="#" class="nav-link nav-link {{ Request::is('admin/PkgCreationTache*') ? 'active' : '' }}">
+        <i class="nav-icon {{__('PkgCreationTache::PkgCreationTache.icon')}}"></i>
+        <p>
+            {{__('PkgCreationTache::PkgCreationTache.name')}}
+            <i class="fas fa-angle-left right"></i>
+        </p>
+    </a>
+    <ul class="nav nav-treeview">
+    </ul>
+</li>
+@endif
+
