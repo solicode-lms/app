89b25199fe7628f73fffb9b6e62106088fd0dc97
ESSARRAJ <essarraj.fouad@gmail.com>
2025-07-25 13:59:57 +0100

    Add PkgSessions module with initial structure and configuration

---
+++ modules/PkgSessions/App/Providers/PkgSessionsServiceProvider.php
@@ -0,0 +1,12 @@
+<?php
+// Ce fichier est maintenu par ESSARRAJ Fouad
+
+
+
+namespace Modules\PkgSessions\App\Providers;
+use Modules\PkgSessions\App\Providers\Base\BasePkgSessionsServiceProvider;
+
+class PkgSessionsServiceProvider extends BasePkgSessionsServiceProvider
+{
+    
+}
