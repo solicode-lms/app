d7f9d4f2fafe7deef5858965226321062e87d14d
ESSARRAJ <essarraj.fouad@gmail.com>
2025-08-06 18:43:48 +0100

    s

--- modules/PkgApprentissage/Services/RealisationUaProjetService.php
+++ modules/PkgApprentissage/Services/RealisationUaProjetService.php
@@ -2,6 +2,8 @@
 
 
 namespace Modules\PkgApprentissage\Services;
+
+use Modules\PkgApprentissage\Models\RealisationUaProjet;
 use Modules\PkgApprentissage\Services\Base\BaseRealisationUaProjetService;
 
 /**
@@ -16,6 +18,26 @@ class RealisationUaProjetService extends BaseRealisationUaProjetService
         if ($realisationUaProjet->wasChanged(['note', 'bareme'])) {
             if ($realisationUaProjet->realisationUa) {
                 (new RealisationUaService())->calculerProgressionEtNote($realisationUaProjet->realisationUa);
+            }
+        }
+
+         // 🔁 2. Recalculer la note de la tâche à partir des realisationUaProjet
+        if ($realisationUaProjet->realisation_tache_id) {
+            $tache = $realisationUaProjet->realisationTache;
+
+            if ($tache) {
+
+                $realisationUaProjets = RealisationUaProjet::where('realisation_tache_id', $tache->id)->get();
+
+                $noteTotale = $realisationUaProjets->sum(function ($proto) {
+                    return min($proto->note ?? 0, $proto->bareme ?? 0);
+                });
+
+                $tache->note = round($noteTotale, 2);
+
+                // Attention si on appelle realisationTacheServiceUpdate, il va lancer la modification
+                // de realisationUaProjet ce qui créer un boucle infinie
+                $tache->save();
             }
         }
     }
