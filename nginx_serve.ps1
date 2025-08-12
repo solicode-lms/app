8212c734bc3fbb70889600cc8519269a01d6d455
ESSARRAJ <essarraj.fouad@gmail.com>
2025-08-12 10:39:23 +0100

    s

---
+++ nginx_serve.ps1
@@ -0,0 +1,16 @@
+# Démarrer Nginx
+cd "C:\nginx\"
+start nginx.exe
+
+# Lancer PHP en arrière-plan
+cd "C:\php\"
+Start-Process "php-cgi.exe" -ArgumentList "-b 127.0.0.1:9000"
+
+# Retourner au dossier du script
+cd $PSScriptRoot
+
+# Attendre un peu pour laisser démarrer les services
+Start-Sleep -Seconds 2
+
+# Ouvrir l'application Laravel
+Start-Process "http://localhost:8080"
