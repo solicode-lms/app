9f2ae20d20a3e8d62eaa1b2ffa4dd3b8e3edfb2d
ESSARRAJ <essarraj.fouad@gmail.com>
2025-08-23 22:45:17 +0100

    s

--- modules/PkgApprentissage/resources/views/realisationModule/custom/fields/progression_cache.blade.php
+++ modules/PkgApprentissage/resources/views/realisationModule/custom/fields/progression_cache.blade.php
@@ -1,5 +1,6 @@
 
- <div class="realisation-etat">
+<div class="realisation-etat"
+     style="--etat-color: {{ $entity->etatRealisationModule->sysColor->hex ?? '#6c757d' }};">
 
      <x-badge 
         :text="Str::limit($entity->etatRealisationModule->nom ?? '', 20)" 
@@ -7,15 +8,15 @@
         class="badge-etat"
     /> 
 
-
-
-    {{-- Progression pédagogique --}}
+     {{-- Progression pédagogique --}}
     <div class="etat-meta" title="Progression" data-toggle="tooltip">
         <x-progression-bar 
             :progression="$entity->progression_cache ?? 0" 
             :progression-ideal="$entity->progression_ideal_cache ?? 0" 
             />
     </div>
+
+   
 
     <span class="etat-meta" title="Rythme" data-toggle="tooltip">
             <i class="far fa-clock"></i> Rythme : {{ $entity->taux_rythme_cache }} %
@@ -27,16 +28,12 @@
         {!! $entity->lecture_pedagogique !!}
     </div>
 
+
+
+
+
     <span class="etat-meta" title="Date de dernière modification" data-toggle="tooltip">
             <i class="far fa-clock"></i> {{ $entity->dernier_update }} 
     </span>
 
 </div>
-
-
-<x-progression-bar 
-    :progression="$entity->progression_cache ?? 0" 
-    :progression-ideal="$entity->progression_ideal_cache ?? 0" 
-    :taux-rythme="$entity->taux_rythme_cache" />
- {!! $entity->lecture_pedagogique !!}
-
