7240a3c0235ffd4a8d7ae38cc8fea3b748806314
= <=>
2026-03-25 13:11:27 +0100

    save

---
+++ docs/8.déploiement/update_28.md
@@ -0,0 +1,7 @@
+
+`````bash
+sudo php artisan db:seed --class=Modules\\PkgApprentissage\\Database\\Seeders\\RealisationModuleSeeder
+````
+
+
+php artisan db:seed --class=Modules\PkgApprentissage\Database\Seeders\RealisationModuleSeeder
--- modules/PkgApprentissage/resources/views/realisationModule/custom/fields/note_cache.blade.php
+++ modules/PkgApprentissage/resources/views/realisationModule/custom/fields/note_cache.blade.php
@@ -1,2 +1,23 @@
-{{ $entity->note_cache }} /
-{{ $entity->bareme_cache }}
+@php
+    $bareme = floatval($entity->bareme_cache) > 0 ? floatval($entity->bareme_cache) : 1;
+    $note = floatval($entity->note_cache);
+    $pourcentage = round(($note / $bareme) * 100, 2);
+    $noteSur40 = round(($note / $bareme) * 40, 2);
+    
+    if ($pourcentage >= 75) {
+        $colorClass = 'success';
+    } elseif ($pourcentage >= 50) {
+        $colorClass = 'warning';
+    } else {
+        $colorClass = 'danger';
+    }
+@endphp
+
+<div class="d-flex flex-column align-items-center" style="min-width: 140px;">
+    <div class="mb-2 w-100 d-flex flex-column align-items-center">
+        <span class="badge badge-primary px-2 py-1 mb-1" title="Note originale">{{ $note }} / {{ $entity->bareme_cache }}</span>
+        <span class="badge badge-info px-2 py-1" title="Note sur 40">{{ $noteSur40 }} / 40</span>
+    </div>
+    
+    <small class="text-{{ $colorClass }} font-weight-bold">{{ $pourcentage }}%</small>
+</div>
