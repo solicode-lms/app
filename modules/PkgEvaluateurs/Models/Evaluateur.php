8afd0956e18d39fe6f657a39c75f706f4897de5d
= <=>
2026-06-11 12:28:34 +0100

    save

--- modules/PkgEvaluateurs/Models/Evaluateur.php
+++ modules/PkgEvaluateurs/Models/Evaluateur.php
@@ -10,8 +10,10 @@ class Evaluateur extends BaseEvaluateur
         'user'
     ];
 
-    public function generateReference(): string
-    {
-         return  $this->user->reference ;
-    }
+
+    // [2026-06-11 11:26:41] developpement.ERROR: Attempt to read property "reference" on null {"userId":1,"exception":"[object] (ErrorException(code: 0): Attempt to read property \"reference\" on null at C:\\AppServer\\solicode-lms\\modules\\PkgEvaluateurs\\Models\\Evaluateur.php:15)
+    // public function generateReference(): string
+    // {
+    //      return  $this->user->reference ;
+    // }
 }
