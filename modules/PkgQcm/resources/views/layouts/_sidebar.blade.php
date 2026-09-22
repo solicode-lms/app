6a242b0439fd1acf0b1bf6ea4b9f71ea49123152
Fouad Systems <fouad.systems@gmail.com>
2026-09-22 21:06:50 +0000

    save

--- modules/PkgQcm/resources/views/layouts/_sidebar.blade.php
+++ modules/PkgQcm/resources/views/layouts/_sidebar.blade.php
@@ -1,16 +1,34 @@
 {{-- Ce fichier est maintenu par ESSARRAJ Fouad --}}
 
 
-@accessiblePermissions(['index-etatRealisationQcm', 'index-qcm', 'index-questionLib', 'index-propositionReponse', 'index-questionQcm', 'index-affectationQcmProjet', 'index-realisationQcm', 'index-reponseQcm'])
+@accessiblePermissions(['index-affectationQcmProjet', 'index-etatRealisationQcm', 'index-propositionReponse', 'index-qcm', 'index-questionLib', 'index-questionQcm', 'index-realisationQcm', 'index-reponseQcm'])
 @if($accessiblePermissions->isNotEmpty())
     @if($accessiblePermissions->count() === 1)
         {{-- Cas d’un seul élément accessible --}}
+            @can('index-affectationQcmProjet')
+            <li class="nav-item" id="menu-affectationQcmProjets">
+                <a href="{{ route('affectationQcmProjets.index') }}" 
+                   class="nav-link {{ Request::is('admin/PkgQcm/affectationQcmProjets') ? 'active' : '' }}">
+                    <i class="nav-icon fas fa-table"></i>
+                    <p>{{__('PkgQcm::affectationQcmProjet.plural')}}</p>
+                </a>
+            </li>
+            @endcan
             @can('index-etatRealisationQcm')
             <li class="nav-item" id="menu-etatRealisationQcms">
                 <a href="{{ route('etatRealisationQcms.index') }}" 
                    class="nav-link {{ Request::is('admin/PkgQcm/etatRealisationQcms') ? 'active' : '' }}">
                     <i class="nav-icon fas fa-table"></i>
                     <p>{{__('PkgQcm::etatRealisationQcm.plural')}}</p>
+                </a>
+            </li>
+            @endcan
+            @can('index-propositionReponse')
+            <li class="nav-item" id="menu-propositionReponses">
+                <a href="{{ route('propositionReponses.index') }}" 
+                   class="nav-link {{ Request::is('admin/PkgQcm/propositionReponses') ? 'active' : '' }}">
+                    <i class="nav-icon fas fa-table"></i>
+                    <p>{{__('PkgQcm::propositionReponse.plural')}}</p>
                 </a>
             </li>
             @endcan
@@ -32,30 +50,12 @@
                 </a>
             </li>
             @endcan
-            @can('index-propositionReponse')
-            <li class="nav-item" id="menu-propositionReponses">
-                <a href="{{ route('propositionReponses.index') }}" 
-                   class="nav-link {{ Request::is('admin/PkgQcm/propositionReponses') ? 'active' : '' }}">
-                    <i class="nav-icon fas fa-table"></i>
-                    <p>{{__('PkgQcm::propositionReponse.plural')}}</p>
-                </a>
-            </li>
-            @endcan
             @can('index-questionQcm')
             <li class="nav-item" id="menu-questionQcms">
                 <a href="{{ route('questionQcms.index') }}" 
                    class="nav-link {{ Request::is('admin/PkgQcm/questionQcms') ? 'active' : '' }}">
                     <i class="nav-icon fas fa-table"></i>
                     <p>{{__('PkgQcm::questionQcm.plural')}}</p>
-                </a>
-            </li>
-            @endcan
-            @can('index-affectationQcmProjet')
-            <li class="nav-item" id="menu-affectationQcmProjets">
-                <a href="{{ route('affectationQcmProjets.index') }}" 
-                   class="nav-link {{ Request::is('admin/PkgQcm/affectationQcmProjets') ? 'active' : '' }}">
-                    <i class="nav-icon fas fa-table"></i>
-                    <p>{{__('PkgQcm::affectationQcmProjet.plural')}}</p>
                 </a>
             </li>
             @endcan
@@ -88,11 +88,27 @@
             </p>
         </a>
         <ul class="nav nav-treeview">
+            @can('index-affectationQcmProjet') 
+            <li class="nav-item" id="menu-affectationQcmProjets">
+                <a href="{{ route('affectationQcmProjets.index') }}" class="nav-link {{ Request::is('admin/PkgQcm/affectationQcmProjets') ? 'active' : '' }}">
+                    <i class="nav-icon fas fa-table"></i>
+                    <p>{{__('PkgQcm::affectationQcmProjet.plural')}}</p>
+                </a>
+            </li>
+            @endcan
             @can('index-etatRealisationQcm') 
             <li class="nav-item" id="menu-etatRealisationQcms">
                 <a href="{{ route('etatRealisationQcms.index') }}" class="nav-link {{ Request::is('admin/PkgQcm/etatRealisationQcms') ? 'active' : '' }}">
                     <i class="nav-icon fas fa-table"></i>
                     <p>{{__('PkgQcm::etatRealisationQcm.plural')}}</p>
+                </a>
+            </li>
+            @endcan
+            @can('index-propositionReponse') 
+            <li class="nav-item" id="menu-propositionReponses">
+                <a href="{{ route('propositionReponses.index') }}" class="nav-link {{ Request::is('admin/PkgQcm/propositionReponses') ? 'active' : '' }}">
+                    <i class="nav-icon fas fa-table"></i>
+                    <p>{{__('PkgQcm::propositionReponse.plural')}}</p>
                 </a>
             </li>
             @endcan
@@ -112,27 +128,11 @@
                 </a>
             </li>
             @endcan
-            @can('index-propositionReponse') 
-            <li class="nav-item" id="menu-propositionReponses">
-                <a href="{{ route('propositionReponses.index') }}" class="nav-link {{ Request::is('admin/PkgQcm/propositionReponses') ? 'active' : '' }}">
-                    <i class="nav-icon fas fa-table"></i>
-                    <p>{{__('PkgQcm::propositionReponse.plural')}}</p>
-                </a>
-            </li>
-            @endcan
             @can('index-questionQcm') 
             <li class="nav-item" id="menu-questionQcms">
                 <a href="{{ route('questionQcms.index') }}" class="nav-link {{ Request::is('admin/PkgQcm/questionQcms') ? 'active' : '' }}">
                     <i class="nav-icon fas fa-table"></i>
                     <p>{{__('PkgQcm::questionQcm.plural')}}</p>
-                </a>
-            </li>
-            @endcan
-            @can('index-affectationQcmProjet') 
-            <li class="nav-item" id="menu-affectationQcmProjets">
-                <a href="{{ route('affectationQcmProjets.index') }}" class="nav-link {{ Request::is('admin/PkgQcm/affectationQcmProjets') ? 'active' : '' }}">
-                    <i class="nav-icon fas fa-table"></i>
-                    <p>{{__('PkgQcm::affectationQcmProjet.plural')}}</p>
                 </a>
             </li>
             @endcan
