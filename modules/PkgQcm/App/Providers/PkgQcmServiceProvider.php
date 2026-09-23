b4afe385c5008ef225f79319ad05f14b08a3b78e
Fouad Systems <fouad.systems@gmail.com>
2026-09-23 21:32:42 +0000

    Create package module

---
+++ modules/PkgQcm/App/Providers/PkgQcmServiceProvider.php
@@ -0,0 +1,12 @@
+<?php
+// Ce fichier est maintenu par ESSARRAJ Fouad
+
+
+
+namespace Modules\PkgQcm\App\Providers;
+use Modules\PkgQcm\App\Providers\Base\BasePkgQcmServiceProvider;
+
+class PkgQcmServiceProvider extends BasePkgQcmServiceProvider
+{
+    
+}
